db70d45b7f4fb2c8037ce6065b747286e0c06858
alexandersokolow <alexandersokolow96@gmail.com>
2024-07-25 05:39:28 +0200

    apply center patch

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static int topbar = 0;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Anonymous Pro:size=12"
+	"Space Mono:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -22,3 +22,6 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 1200;                    /* minimum width when centered */
